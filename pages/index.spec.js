6a3892e58a47a127074187050151582ed3353d75
Sebastian Landwehr <info@dword-design.de>
2021-02-25 10:22:51 +0100

    fix: fix

--- pages/index.spec.js
+++ pages/index.spec.js
@@ -3,17 +3,8 @@ import tester from '@dword-design/tester'
 import testerPluginNuxt from '@dword-design/tester-plugin-nuxt'
 import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer'
 import { toMatchImage } from 'jest-image-matcher'
-import nodemailer from 'nodemailer'
 import P from 'path'
 
-const transport = nodemailer.createTransport({
-  auth: {
-    pass: 'e49e4f04c1fd9d',
-    user: '42d4583fb25b92',
-  },
-  host: 'smtp.mailtrap.io',
-  port: 2525,
-})
 expect.extend({ toMatchImage })
 
 export default tester(
@@ -25,16 +16,6 @@ export default tester(
       const screenshot = await this.page.screenshot({
         fullPage: true,
         // path: P.join(__dirname, '-fixtures', 'add-row.png'),
-      })
-      await transport.sendMail({
-        attachments: {
-          content: screenshot,
-          contentType: 'image/png',
-          filename: 'screenshot.png',
-        },
-        from: 'a@b.de',
-        subject: 'Screenshot',
-        to: 'info@dword-design.de',
       })
       expect(screenshot).toMatchImage(
         P.join(__dirname, '-fixtures', 'add-row.png'),
